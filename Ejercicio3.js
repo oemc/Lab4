1a189a7847bb540fca2b8f95375643d32a7133c9
Oscar Morales <oemc_2404@outlook.com>
2018-09-26 19:02:16 -0600

    Lab3 ejercio 3 con archivo

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,5 +1,95 @@
-function parentesis() {
-    return true;
+function verificar(input){
+    console.log('verificando: ' + input);
+    let chars = input.split('');
+    let position = 0;
+    while(position < chars.length){
+        switch(chars[position]){
+            case '(':
+                position = parentesis(chars, position+1);
+                break;
+            case '{':
+                position = llaves(chars, position+1);
+                break;
+            case '[':
+                position = corchetes(chars, position+1);
+                break;
+            case ')':
+            case '}':
+            case ']':
+                throw('Error en caracter ' + position);
+        }
+        position++;
+    }
+    return(true);
 }
 
-console.log(parentesis());
+function parentesis(chars, position){
+    while(position < chars.length){
+        switch(chars[position]){
+            case '(':
+                position = parentesis(chars, position+1);
+                break;
+            case '{':
+                position = llaves(chars, position+1);
+                break;
+            case '[':
+                position = corchetes(chars, position+1);
+                break;
+            case ')':
+                return(position);
+            case '}':
+            case ']':
+                throw('Error en caracter ' + position + '. Se esperaba )');
+        }
+        position++;
+    }
+    throw('Error en caracter ' + position + '. Se esperaba )');
+}
+
+function llaves(chars, position){
+    while(position < chars.length){
+        switch(chars[position]){
+            case '(':
+                position = parentesis(chars, position+1);
+                break;
+            case '{':
+                position = llaves(chars, position+1);
+                break;
+            case '[':
+                position = corchetes(chars, position+1);
+                break;
+            case '}':
+                return(position);
+            case ')':
+            case ']':
+                throw('Error en caracter ' + position + '. Se esperaba }');
+        }
+        position++;
+    }
+    throw('Error en caracter ' + position + '. Se esperaba }');
+}
+
+function corchetes(chars, position){
+    while(position < chars.length){
+        switch(chars[position]){
+            case '(':
+                position = parentesis(chars, position+1);
+                break;
+            case '{':
+                position = llaves(chars, position+1);
+                break;
+            case '[':
+                position = corchetes(chars, position+1);
+                break;
+            case ']':
+                return(position);
+            case ')':
+            case '}':
+                throw('Error en caracter ' + position + '. Se esperaba ]');
+        }
+        position++;
+    }
+    throw('Error en caracter ' + position + '. Se esperaba ]');
+}
+
+console.log(verificar('[([{}])]{()}'));

ee8bfca435a4c125f64682825f215649a7eab8a9
Oscar Morales <oemc_2404@outlook.com>
2018-09-28 12:35:00 -0600

    Server Hello

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var http = require('http');
+var parser = require('url');
+
 const port = 3000;
 
 const requestHandler = (req, res) => {
-    console.log(req.url);
-    res.end('Hello World');
+    let url = parser.parse(req.url, true);
+    let route = url.pathname;
+    console.log(route);
+    switch (route){
+        case (route.match(/^\/hello\//i) || {}).input:
+            let json = {"hello": route.substring(7,route.length)};
+            res.end(JSON.stringify(json));
+            break;
+        default:
+            res.end('Not found');
+            break;
+    }
 }
 
 const server = http.createServer(requestHandler);
